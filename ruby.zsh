83f4c0acfa2b42855dd72fb6a1e73516789336bd
Matt Sturgeon <matt@sturgeon.me.uk>
2017-07-19 00:01:23 +0100

    Fix ruby bundler installing to /usr
    Needed to set GEM_HOME.

--- ruby.zsh
+++ ruby.zsh
@@ -1,2 +1,5 @@
+# Set bundler to install in the user directory, not system wide
+export GEM_HOME=$(ruby -e 'print Gem.user_dir')
+
 # Add gems to PATH
-export PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
+export PATH="$GEM_HOME/bin:$PATH"
